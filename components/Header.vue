91a6fb1f5616cbf187623b05ff026c51ef945201
RafiAriel <rafiariel000@gmail.com>
2024-06-03 00:29:33 +0300

    header kickoff

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="text-green-500">welcome</div>
+  <div class="flex bg-gray-300 justify-between p-4">
+    <div></div>
+    <div class="logo">
+      <NuxtLink to="/">Home</NuxtLink>
+
+      <!--replace it with actual logo. this is home-->
+    </div>
+
+    <div class="flex justify-between gap-2">
+      <NuxtLink to="/temp">temp</NuxtLink>
+
+      <!--find how to set css from nuxt - 'active' -->
+
+      <div>
+        user icon
+        <!--replace it with actual icon-->
+      </div>
+
+      <div>
+        cart icon
+
+        <!--replace it with actual icon-->
+      </div>
+
+      <div>
+        contact us icon
+
+        <!--replace it with actual icon-->
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
+<script scoped>
 export default {
   name: "Header",
 };
