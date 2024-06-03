3d446fd152f00f2c05867eade5bb3424b7b43989
RafiAriel <rafiariel000@gmail.com>
2024-06-03 18:00:27 +0300

    header completed

--- components/Header.vue
+++ components/Header.vue
@@ -1,28 +1,18 @@
 <template>
-  <div class="flex bg-gray-300 justify-between p-4">
+  <div
+    class="flex bg-gray-300 justify-between p-4 sticky top-0 border-b-2 border-indigo-400 border-opacity-50"
+  >
     <div class="text-lg font-bold tracking-widest leading-loose">
       <NuxtLink to="/">HOME</NuxtLink>
     </div>
 
-    <div class="flex justify-between gap-2">
-      <NuxtLink to="/temp">temp</NuxtLink>
-
+    <div class="flex justify-between gap-5">
       <div>
-        cart icon
-
-        <!--replace it with actual icon-->
+        <CartIcon />
       </div>
 
       <div>
-        contact us icon
-
-        <!--replace it with actual icon-->
-      </div>
-      <div>
-        user icon
         <UserIcon />
-
-        <!--replace it with actual icon-->
       </div>
     </div>
   </div>
@@ -30,10 +20,13 @@
 
 <script scoped>
 import UserIcon from "../icons/UserIcon.vue";
+import CartIcon from "../icons/CartIcon.vue";
+
 export default {
   name: "Header",
   components: {
     UserIcon: UserIcon,
+    CartIcon: CartIcon,
   },
 };
 </script>
