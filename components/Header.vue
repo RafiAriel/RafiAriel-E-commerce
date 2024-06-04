493f6020effa36ba0d143f448ffc29982fece6a4
RafiAriel <rafiariel000@gmail.com>
2024-06-05 01:34:54 +0300

    minor css fix

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex bg-gray-300 justify-between p-4 sticky top-0 border-b-2 border-indigo-400 border-opacity-50"
+    class="flex bg-gray-300 justify-between p-4 sticky top-0 border-b-2 border-indigo-400 border-opacity-50 mb-20"
   >
     <div class="text-lg font-bold tracking-widest leading-loose">
       <NuxtLink to="/">HOME</NuxtLink>
