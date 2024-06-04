9adfd24118261a88065abef62390f3dbecbeed7a
RafiAriel <rafiariel000@gmail.com>
2024-06-05 01:36:01 +0300

    continue fill details of product component

---
+++ components/Product.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="grid grid-cols-1 gap-2 lg:gap-4">
+    <img
+      class="w-24 lg:w-48 h-24 lg:h-48 justify-self-center"
+      :src="product.image"
+    />
+    <div class="text-sm lg:text-base text-center truncate">
+      {{ product.title }}
+    </div>
+    <div class="font-bold text-lg text-red-600">
+      {{ `$${product.price}` }}
+    </div>
+
+    <!-- 
+      https://www.w3schools.com/howto/howto_css_star_rating.asp
+    -->
+  </div>
+</template>
+
+<script scoped>
+export default {
+  name: "Product",
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
-  <div></div>
+  <div class="grid grid-cols-2 gap-12 justify-items-center p-5 lg:p-10">
+    <div v-for="(product, index) in products" :key="index">
+      <Product :product="product" />
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import Product from "../components/Product.vue";
+
 export default {
   name: "IndexPage",
+  components: {
+    Product: Product,
+  },
+  computed: {
+    ...mapGetters({
+      products: "products/getProducts",
+    }),
+  },
 };
 </script>
