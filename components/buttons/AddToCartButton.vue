d11c32751ba00edc090602ba4491cc7e0d25a045
RafiAriel <rafiariel000@gmail.com>
2024-06-07 00:04:57 +0300

    add hover style to the button

--- components/buttons/AddToCartButton.vue
+++ components/buttons/AddToCartButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex items-center gap-2 p-2 lg:p-4 bg-gray-300 rounded-full border-opacity-50"
+    class="flex items-center gap-2 p-2 lg:p-4 bg-gray-300 rounded-full border-opacity-50 hover:bg-gray-600 hover:text-white"
   >
     <i class="fa-solid fa-cart-plus"></i>
     <div class="text-sm lg:text-base">ADD TO CART</div>
