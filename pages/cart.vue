fdc5006e4950a0b3c09e8278594fad939f1de294
RafiAriel <rafiariel000@gmail.com>
2024-06-07 17:05:22 +0300

    implement show now button in case empty cart

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>Your cart is empty</div>
-  <!--Add button to the product page-->
+  <div
+    class="flex flex-col gap-4 lg:gap-8 justify-center items-center text-center py-20 lg:py-30 font-bold text-xl lg:text-4xl bg-gray-300 bg-opacity-80"
+  >
+    <div>Your cart is empty</div>
+
+    <NuxtLink to="/"> <ShopNowButton /> </NuxtLink>
+  </div>
 </template>
 
 <script>
+import ShopNowButton from "../components/buttons/ShopNowButton.vue";
 export default {
   name: "Cart",
+  components: {
+    ShopNowButton: ShopNowButton,
+  },
 };
 </script>
