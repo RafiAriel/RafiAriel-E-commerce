ed29a120d904e0685865fb25653ec90ccd288ac0
RafiAriel <rafiariel000@gmail.com>
2024-06-07 17:04:39 +0300

    style show now button

---
+++ components/buttons/ShopNowButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    class="p-3 lg:p-6 bg-white rounded-full border-opacity-50 hover:bg-black hover:text-white"
+  >
+    <div class="text-sm lg:text-base">Continue Shopping</div>
+  </button>
+</template>
+
+<script scoped>
+export default {
+  name: "ShopNowButton",
+};
+</script>
+
+<style scoped></style>
