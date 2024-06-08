db1fa4131283f39efea7053bf3edba7da88f3414
RafiAriel <rafiariel000@gmail.com>
2024-06-09 02:39:47 +0300

    quantity section completed

---
+++ components/Quantity.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex gap-2">
+    <button @click="increase()">
+      <i class="fa-solid fa-circle-plus"></i>
+    </button>
+    <input
+      class="w-26 text-center text-lg border-solid border-2 rounded border-gray-300"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      type="text"
+    />
+    <button @click="decrease()" :disabled="disableDecreaseButton">
+      <i class="fa-solid fa-circle-minus"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quantity",
+  props: ["value"],
+  methods: {
+    increase() {
+      this.value++;
+      this.$emit("input", this.value);
+    },
+    decrease() {
+      if (this.value === 1) return;
+      this.value--;
+      this.$emit("input", this.value);
+    },
+  },
+  computed: {
+    disableDecreaseButton() {
+      return this.value === 1;
+    },
+  },
+};
+</script>
--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -1,21 +1,24 @@
 <template>
-  <div class="grid grid-cols-2 gap-12 justify-items-center p-5 lg:p-10">
+  <div class="grid grid-cols-1 gap-6 justify-items-center p-5 lg:p-10">
     <Product :product="chosenProduct" />
+    <Quantity v-model="productQuantity" />
   </div>
 </template>
 
 <script>
 import Product from "../../components/Product.vue";
-
+import Quantity from "../../components/Quantity.vue";
 export default {
   name: "product",
   components: {
     Product: Product,
+    Quantity: Quantity,
   },
   async asyncData({ params, store }) {
     const products = store.getters["products/getProducts"];
     const chosenProduct = products.find((p) => p.id === Number(params.slug));
-    return { chosenProduct };
+    let productQuantity = 1;
+    return { chosenProduct, productQuantity };
   },
 };
 </script>
