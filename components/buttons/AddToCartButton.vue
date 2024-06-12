46a62a5aa5c44a3a085b7caa146a5e7defc7c373
RafiAriel <rafiariel000@gmail.com>
2024-06-13 02:08:25 +0300

    add state hover to add to cart btn

--- components/buttons/AddToCartButton.vue
+++ components/buttons/AddToCartButton.vue
@@ -2,7 +2,7 @@
   <button
     v-bind="$attrs"
     v-on="$listeners"
-    class="flex items-center gap-2 p-2 lg:p-4 bg-gray-300 rounded-full border-opacity-50 hover:bg-gray-600 hover:text-white"
+    class="flex items-center gap-2 p-2 lg:p-4 bg-gray-300 rounded-full border-opacity-50 hover:bg-indigo-400 hover:text-white"
   >
     <i class="fa-solid fa-cart-plus"></i>
     <div class="text-sm lg:text-base">ADD TO CART</div>
