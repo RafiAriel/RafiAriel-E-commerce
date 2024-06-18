eedbce2937e2106e8d1ebea2e514314f24f56e16
RafiAriel <rafiariel000@gmail.com>
2024-06-19 00:01:12 +0300

    add new icon

---
+++ components/buttons/CircleRightButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>
+    <i class="fa-solid fa-circle-right"></i>
+  </button>
+</template>
+
+<script scoped>
+export default {
+  name: "CircleRightButton",
+};
+</script>
+
+<style scoped></style>
