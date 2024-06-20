3b6cce8339e37ec0a8c2fc773af0316d5618fd2b
RafiAriel <rafiariel000@gmail.com>
2024-06-20 10:37:09 +0300

    add icons

---
+++ icons/GridIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <i class="fa-solid fa-table-cells-large"></i>
+</template>
+
+<script scoped>
+export default {
+  name: "GridIcon",
+};
+</script>
+
+<style scoped></style>
---
+++ icons/SwiperViewIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <i class="fa-solid fa-right-left"></i>
+    <i class="fa-regular fa-hand-spock"></i>
+  </div>
+</template>
+
+<script scoped>
+export default {
+  name: "SwiperViewIcon",
+};
+</script>
+
+<style scoped></style>
