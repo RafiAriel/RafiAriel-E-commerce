5a0b8a1671d80a1c583e2f0fdef62552b8537916
RafiAriel <rafiariel000@gmail.com>
2024-06-25 16:28:16 +0300

    add new spinner ring

---
+++ components/SpinnerRing.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="w-12 h-12 rounded-full animate-spin border-4 border-solid border-sky-500 border-t-transparent"
+  ></div>
+</template>
+
+<script scoped>
+export default {
+  name: "SpinnerRing",
+};
+</script>
+
+<style scoped></style>
